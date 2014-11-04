0e6b5883ce0847fabe6bc6f726a945cc6ee17fd7
Bourinator <egillorn89@gmail.com>
2014-11-04 20:06:30 +0000

    honnun

--- BlissAppid_git/BlissAppid/app/src/main/java/com/example/seventeen/blissappid/Bua_til_toflu.java
+++ BlissAppid_git/BlissAppid/app/src/main/java/com/example/seventeen/blissappid/Bua_til_toflu.java
@@ -2,8 +2,17 @@ package com.example.seventeen.blissappid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 
 public class Bua_til_toflu extends Activity {
@@ -14,6 +23,28 @@ public class Bua_til_toflu extends Activity {
         setContentView(R.layout.activity_bua_til_toflu);
     }
 
+    public void displayOutput()
+    {
+        File sdcard = Environment.getExternalStorageDirectory();
+        File file = new File(sdcard,"/TextFile.txt");
+        StringBuilder text = new StringBuilder();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = br.readLine()) != null) {
+                text.append(line);
+                text.append('\n');
+            }
+        }
+        catch (IOException e) {
+            Toast.makeText(getApplicationContext(), "Error reading file!", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+
+        TextView output=(TextView) findViewById(R.id.output);
+        // Assuming that 'output' is the id of your TextView
+        output.setText(text);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
