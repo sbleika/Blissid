efac9a988cdf9952ae116d8bb7bffcd5617e4b78
Bourinator <egillorn89@gmail.com>
2014-11-05 14:37:52 +0000

    JavaDoc add og meiri skjölun

--- BlissAppid_git/BlissAppid/app/src/main/java/com/example/seventeen/blissappid/starfsmadur1.java
+++ BlissAppid_git/BlissAppid/app/src/main/java/com/example/seventeen/blissappid/starfsmadur1.java
@@ -8,13 +8,17 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 /**
- * Höfunur: Egill Örn Sigþórsson
+ * Hofunur: Egill Orn Sigthosson
  * Dagsetning: 28.10.2014
- * Lýsing:
+ * Lysing: vidmot fyrir starfsmann tar sem hann getur valid
+ * um ad fara í notkunarskra eda ad bua til toflu
  */
 
 public class starfsmadur1 extends Activity {
-
+    /**
+     * Gerum onclicklisner fyrir bua til toflu takka  og notkunarskra takka
+     * @param savedInstanceState save instance
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,41 +32,65 @@ public class starfsmadur1 extends Activity {
         buatiltolfubutton.setOnClickListener(gotobuatiltofluClickListener);
     }
     View.OnClickListener gotoNotkunarskraClickListener = new View.OnClickListener() {
-
+        /**
+         * ef smellt er a notkunarskra takkan er kallad a fallid Nbutton()
+         * @param v view
+         */
         @Override
         public void onClick(View v) {
-
             Nbutton();
         }
     };
+
+    /**
+     * opnar nyja gluggan Notkunarskra
+     */
     private void Nbutton(){
         startActivity(new Intent(this, Notkunarskra.class));
     }
 
 
     View.OnClickListener gotobuatiltofluClickListener = new View.OnClickListener() {
-
+        /**
+         * ef smellt er a bua til toflu takkan er kallad a fallid BTTbutton()
+         * @param v view
+         */
         @Override
         public void onClick(View v) {
 
             BTTbutton();
         }
     };
+
+    /**
+     * opnar nyja gluggan Bua til toflu
+     */
     private void BTTbutton(){
         startActivity(new Intent(this, Bua_til_toflu.class));
     }
+
+    /**
+     * Inflate the menu; this adds items to the action bar if it is present.
+     * @param menu menu
+     * @return true
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+
         getMenuInflater().inflate(R.menu.starfsmadur1, menu);
         return true;
     }
 
+    /**
+     * Handle action bar item clicks here. The action bar will
+     * automatically handle clicks on the Home/Up button, so long
+     * as you specify a parent activity in AndroidManifest.xml.
+     * @param item menuitem
+     * @return super.onOptionsItemSelected(item);
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
