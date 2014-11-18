304f3a4008c46c6855e83b86ec934759491c956e
Bourinator <egillorn89@gmail.com>
2014-11-18 11:17:48 +0000

    maketable

---
+++ BlissAppid_git/BlissAppid/app/src/main/java/com/example/seventeen/blissappid/MakeTable.java
@@ -0,0 +1,11 @@
+package com.example.seventeen.blissappid;
+
+/**
+ * Created by EOS on 11/18/2014.
+ */
+public class MakeTable {
+
+    public static boolean maketable(int x) {
+        return true;
+    }
+}
