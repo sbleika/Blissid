e2a56f4588cd9036493f2a2c8b0dc421facd56c3
Bourinator <egillorn89@gmail.com>
2014-11-18 12:32:42 +0000

    set gone

--- BlissAppid_git/BlissAppid/app/src/main/java/com/example/seventeen/blissappid/Bua_til_toflu.java
+++ BlissAppid_git/BlissAppid/app/src/main/java/com/example/seventeen/blissappid/Bua_til_toflu.java
@@ -38,36 +38,82 @@ public class Bua_til_toflu extends Activity {
         maketable = (Button) findViewById(R.id.maketable);
         maketable.setOnClickListener(gotomaketableClickListener);
 
-
-
+        String txt9=upphafsmynd.getText().toString();
 
     }
+
+    EditText upphafsmynd = (EditText) findViewById(R.id.textView9);
+    EditText nr1 = (EditText) findViewById(R.id.editText4);
+    EditText nr2 = (EditText) findViewById(R.id.editText5);
+    EditText nr3 = (EditText) findViewById(R.id.editText7);
+    EditText nr4 = (EditText) findViewById(R.id.editText6);
+    EditText nr5 = (EditText) findViewById(R.id.editText9);
+    EditText nr6 = (EditText) findViewById(R.id.editText8);
+    EditText nr7 = (EditText) findViewById(R.id.editText10);
+    EditText nr8 = (EditText) findViewById(R.id.editText11);
+    EditText nr9 = (EditText) findViewById(R.id.editText12);
+    EditText nr10 = (EditText) findViewById(R.id.editText13);
+    EditText nr11 = (EditText) findViewById(R.id.editText14);
+    EditText nr12 = (EditText) findViewById(R.id.editText15);
+
+
+    public void onupphafsmyndClicked(View view) {
+        upphafsmynd.setText("");
+    }
+
+    String _Radiochecked = "0";
 
     public void onRadioButtonClicked(View view) {
         // Is the button now checked?
         boolean checked = ((RadioButton) view).isChecked();
-        EditText upphafsmynd;
-        upphafsmynd = (EditText) findViewById(R.id.textView9);
-        String txt9=upphafsmynd.getText().toString();
+
         // Check which radio button was clicked
         switch(view.getId()) {
             case R.id.radioButton:
                 if (checked) {
-                    upphafsmynd.setText("fjorir");
+                    _Radiochecked = "4";
+                    nr5.setVisibility(View.GONE);
+                    nr6.setVisibility(View.GONE);
+                    nr7.setVisibility(View.GONE);
+                    nr8.setVisibility(View.GONE);
+                    nr9.setVisibility(View.GONE);
+                    nr10.setVisibility(View.GONE);
+                    nr11.setVisibility(View.GONE);
+                    nr12.setVisibility(View.GONE);
                     break;
                 }
             case R.id.radioButton2:
-                if (checked)
-                    // Ninjas rule
+                if (checked) {
+                    _Radiochecked = "6";
+                    nr7.setVisibility(View.GONE);
+                    nr8.setVisibility(View.GONE);
+                    nr9.setVisibility(View.GONE);
+                    nr10.setVisibility(View.GONE);
+                    nr11.setVisibility(View.GONE);
+                    nr12.setVisibility(View.GONE);
                     break;
+                }
             case R.id.radioButton5:
-                if (checked)
-                    // Ninjas rule
+                if (checked) {
+                    _Radiochecked = "9";
+                    nr10.setVisibility(View.GONE);
+                    nr11.setVisibility(View.GONE);
+                    nr12.setVisibility(View.GONE);
                     break;
+                }
             case R.id.radioButton6:
-                if (checked)
-                    // Ninjas rule
+                if (checked) {
+                    _Radiochecked = "12";
+                    nr5.setVisibility(View.VISIBLE);
+                    nr6.setVisibility(View.VISIBLE);
+                    nr7.setVisibility(View.VISIBLE);
+                    nr8.setVisibility(View.VISIBLE);
+                    nr9.setVisibility(View.VISIBLE);
+                    nr10.setVisibility(View.VISIBLE);
+                    nr11.setVisibility(View.VISIBLE);
+                    nr12.setVisibility(View.VISIBLE);
                     break;
+                }
         }
     }
 
