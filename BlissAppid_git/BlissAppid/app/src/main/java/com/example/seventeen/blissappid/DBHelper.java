fc37aac74beb23a735e31a95d471db0a23c9c90a
Ívar Örn Ragnarsson <ior3@hi.is>
2014-11-25 21:20:37 +0000

    added debug print in DBH

--- BlissAppid_git/BlissAppid/app/src/main/java/com/example/seventeen/blissappid/DBHelper.java
+++ BlissAppid_git/BlissAppid/app/src/main/java/com/example/seventeen/blissappid/DBHelper.java
@@ -110,7 +110,7 @@ private static SQLiteDatabase db = null;
             statement.setString(1, symbolName);
             */
             System.err.println("before cursor def");
-            Cursor cursor = db.query("symbols",new String[]{"image"},"_id=\"?\"",new String[]{symbolName},null,null,null,null);
+            Cursor cursor = db.query("symbols",/*new String[]{"image"}*/ null,"_id=\"?\"",new String[]{symbolName},null,null,null,null);
             System.err.println("after cursor def");
 
             System.err.println(cursor.getColumnNames());
