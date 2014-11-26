28e33eb499981da30989ca990542a9af79604bd7
Ívar Örn Ragnarsson <ior3@hi.is>
2014-11-26 22:06:06 +0000

    should be able to make full bigtable now

--- BlissAppid_git/BlissAppid/app/src/main/assets/DBInserter.py
+++ BlissAppid_git/BlissAppid/app/src/main/assets/DBInserter.py
@@ -1,5 +1,12 @@
 import sqlite3;
 
+def find_nth(string, sub, n):
+    start = string.find(sub)
+    while start >= 0 and n > 1:
+       start = string.find(sub, start+len(sub))
+       n -= 1
+    return start
+
 #sql connection stuff
 conn=sqlite3.connect("symbols.db")
 cursor=conn.cursor()
@@ -20,6 +27,9 @@ while currentLine:
     startOfName=currentLine.rfind("/")+1
     endOfName=currentLine.rfind(".")
     name = unicode(currentLine[startOfName:endOfName])
+    startOfTableName = find_nth(currentLine,"/",6)+1;
+    tableName = currentLine[startOfTableName:startOfTableName+3]
+    print tableName
     
     #blob=unicode("readfile(\""+currentLine+"\")")
     imageFile=open(currentLine)
@@ -29,7 +39,7 @@ while currentLine:
     print currentLine
     print name
     
-    cursor.execute ("INSERT INTO bigtable values (1.1,?,?);", (sqlite3.Binary(blob),name))
+    cursor.execute ("INSERT INTO bigtable values (?,?,?);", (tableName,sqlite3.Binary(blob),name))
     currentLine = nafnaListi.readline()
    
 
